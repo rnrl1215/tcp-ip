8054b07815af46a77a748708bd980b5ec43c63d8
skahn <d1215@naver.com>
2021-01-21 23:35:47 +0900

    added uechoserver

--- ex8_udpecho/uecho_server.c
+++ ex8_udpecho/uecho_server.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
@@ -9,7 +9,7 @@
 
 void error_handling(char *message);
 
-int main( argc, char *argv[])
+int main( int argc, char *argv[])
 {
     int serv_sock;
     char message[BUF_SIZE];
@@ -22,10 +22,35 @@ int main( argc, char *argv[])
         exit(1);
     }
 
-    serv_sock = sockt(PF_INET, SOCK_DGRAM, 0);
+    serv_sock = socket(PF_INET, SOCK_DGRAM, 0);
     if( serv_sock == -1)
         error_handling("UDP socket creation error");
 
-    memset("&serv_adr, 0, size
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_adr.sin_port = htons(atoi(argv[1]));
+   
+    if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
+        error_handling("bind() error");
+
+    while(1)
+    { 
+        clnt_adr_sz = sizeof( clnt_adr );
+        str_len = recvfrom( serv_sock, message, BUF_SIZE, 0, 
+                          (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
+        sendto(serv_sock, message, str_len, 0, (struct sockaddr*)&clnt_adr, clnt_adr_sz);
+    }
+
+    close(serv_sock);
+    return 0;
 }
+
+void error_handling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+
 
